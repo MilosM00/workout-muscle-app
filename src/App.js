57c71cb206ba8937d1261d87cf29d6e5476eeefc
Milos Milijasevic <milosrm20005@gmail.com>
2022-08-23 10:48:12 +0200

    app-js updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Muscles from "./components/Muscles";
+import Navbar from "./components/Navbar";
 
 const App = () =>{
     return(
         <div>
             
+            <Navbar />
             <Muscles />
 
         </div>
