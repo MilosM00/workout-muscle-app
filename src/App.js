e29b8de5db90d3e1da661ccdb31a0d395c5e943b
Milos Milijasevic <milosrm20005@gmail.com>
2022-08-24 09:35:13 +0200

    app-js updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React from "react";
+import Chest from "./components/Chest";
 import Muscles from "./components/Muscles";
 import Navbar from "./components/Navbar";
 
 const App = () =>{
+
+    const [muscles, setMuscles] = React.useState(true);
+    const [chest, setChest] = React.useState(false);
+
+    const scrollUp = () =>{
+        document.body.scrollIntoView();
+        setMuscles(true);
+        setChest(false);
+    };
+
+    const chestEvent = () =>{
+        setMuscles(false);
+        setChest(true);
+    };
+
     return(
         <div>
             
-            <Navbar />
-            <Muscles />
+            <Navbar 
+                scrollUp={scrollUp}
+            />
+
+            {muscles === true && 
+                <Muscles 
+                    chestEvent={chestEvent}
+                />
+            }
+
+            {chest === true && 
+                <Chest />
+            }
 
         </div>
     );
