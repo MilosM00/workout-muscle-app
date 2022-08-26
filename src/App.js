538c09a70d282e0b8b62304ae1da61a7e9955cab
Milos Milijasevic <milosrm20005@gmail.com>
2022-08-26 11:01:29 +0200

    app-js updated

--- src/App.js
+++ src/App.js
@@ -2,21 +2,31 @@ import React from "react";
 import Chest from "./components/Chest";
 import Muscles from "./components/Muscles";
 import Navbar from "./components/Navbar";
+import Abdominals from "./components/Abdominals";
 
 const App = () =>{
 
     const [muscles, setMuscles] = React.useState(true);
     const [chest, setChest] = React.useState(false);
+    const [abdominals, setAbdominals] = React.useState(false);
 
     const displayMuscles = () =>{
         document.body.scrollIntoView();
         setMuscles(true);
         setChest(false);
+        setAbdominals(false);
     };
 
     const displayChest = () =>{
-        setMuscles(false);
         setChest(true);
+        setMuscles(false);
+        setAbdominals(false);
+    };
+
+    const displayAbdominals = () =>{
+        setAbdominals(true);
+        setMuscles(false);
+        setChest(false);
     };
 
     return(
@@ -29,10 +39,19 @@ const App = () =>{
             {muscles === true && 
                 <Muscles 
                     displayChest={displayChest}
+                    displayAbdominals={displayAbdominals}
                 />
             }
 
-            {chest === true && <Chest />}
+            {chest === true && 
+                <Chest 
+                    displayMuscles={displayMuscles}
+                />
+            }
+
+            {abdominals === true && 
+                <Abdominals />
+            }
 
         </div>
     );
