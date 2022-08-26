0da6e5ebe62760416816ab1082fdaaf6af9ee07d
Milos Milijasevic <milosrm20005@gmail.com>
2022-08-26 11:03:17 +0200

    abdominals-js updated

---
+++ src/components/Abdominals.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../style/Abdominals.css";
+
+const Abdominals = () =>{
+    return(
+
+        <div className="abs">
+
+        </div>
+
+    );
+};
+
+export default Abdominals;
