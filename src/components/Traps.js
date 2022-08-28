c8f7fb2c240d4bb94a740296adac31e717175762
Milos Milijasevic <milosrm20005@gmail.com>
2022-08-28 10:44:26 +0200

    traps-js updated

---
+++ src/components/Traps.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../style/Traps.css";
+
+const Traps = () =>{
+    return(
+
+        <div className="traps">
+            
+        </div>
+
+    );
+};
+
+export default Traps;
