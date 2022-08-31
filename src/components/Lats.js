756cd28c53df520e3c65fc5778d828ee8b704949
Milos Milijasevic <milosrm20005@gmail.com>
2022-08-31 08:59:10 +0200

    lats-js updated

---
+++ src/components/Lats.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../style/Lats.css";
+
+const Lats = ({displayMuscles}) =>{
+    return(
+
+        <div className="lats">
+
+        </div>
+
+    );
+};
+
+export default Lats;
