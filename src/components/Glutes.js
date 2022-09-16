f20fe8973afc2a18cdfa27293e7991383889451f
Milos Milijasevic <milosrm20005@gmail.com>
2022-09-16 21:49:23 +0200

    glutes-js updated

--- src/components/Glutes.js
+++ src/components/Glutes.js
@@ -16,8 +16,8 @@ const Glutes = ({displayMuscles}) =>{
             </div>
 
             <div className="inline-glutes">
-                <img src="" alt="glutes" height="300" width="400" />
-                <img src="" alt="glutes" height="300" width="400" />
+                <img src="https://qph.cf2.quoracdn.net/main-qimg-045cebcdaaaa5029198cc6c6f64bd29f" alt="glutes" height="300" width="400" />
+                <img src="https://i.imgur.com/IBLtK6y.gif?noredirect" alt="glutes" height="300" width="400" />
             </div>
 
             <div className="info-glutes">
@@ -28,8 +28,8 @@ const Glutes = ({displayMuscles}) =>{
             </div>
 
             <div className="inline-glutes">
-                <img src="" alt="glutes" width="400" height="350" />
-                <img src="" alt="glutes" width="400" height="350" />
+                <img src="https://i.pinimg.com/originals/be/62/6e/be626e0cfe7063c6afcf429d67bbc508.gif" alt="glutes" width="400" height="350" />
+                <img src="https://images.squarespace-cdn.com/content/v1/5b99e55396d455ee78c764fc/1568762859950-US1PZ3YGX2IS8HAODBKI/one-leg-deadlift.gif" alt="glutes" width="400" height="350" />
             </div>
 
             <div className="info-glutes">
@@ -40,8 +40,8 @@ const Glutes = ({displayMuscles}) =>{
             </div>
 
             <div className="inline-glutes">
-                <img src="" alt="glutes" width="400" height="350"/>
-                <img src="" alt="glutes" width="500" height="350" />
+                <img src="https://i.pinimg.com/originals/bd/78/80/bd78802d06f3a74df2311a66e9b89a34.gif" alt="glutes" width="400" height="350"/>
+                <img src="https://hips.hearstapps.com/ame-prod-menshealth-assets.s3.amazonaws.com/main/assets/bodyweight-squat.gif?resize=480:*" alt="glutes" width="500" height="350" />
             </div>
 
             <button 
